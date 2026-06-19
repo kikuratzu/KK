cee93145b219bc855b33c2449e88de0abb56f9ef
kikuratzu <kirilkrushev@gmail.com>
2026-06-19 21:00:10 +0300

    test

--- K&K/K-K/src/main/java/K/K/Repositories/VerificationCodeRepository.java
+++ K&K/K-K/src/main/java/K/K/Repositories/VerificationCodeRepository.java
@@ -2,10 +2,11 @@ package K.K.Repositories;
 
 import K.K.Entities.VerificationCode;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface VerificationCodeRepository extends CrudRepository<VerificationCode, String> {
 Optional<VerificationCode> findById(String code);
 }
